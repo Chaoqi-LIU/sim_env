b4eb88ff2260e7be94c36cfe4ad4d31380be4e74
Chaoqi-LIU <liuchaoqi730@gmail.com>
2025-11-25 11:32:49 -0500

    upload perception/

---
+++ policy_research/perception/base_obs_encoder.py
@@ -0,0 +1,20 @@
+from policy_research.model.common.module_attr_mixin import ModuleAttrMixin
+from policy_research.model.common.normalizer import LinearNormalizer
+from typing import List, Dict, Union
+
+
+class BaseObservationEncoder(ModuleAttrMixin):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, obs_dict: Union[Dict, List[Dict]]) -> Dict:
+        raise NotImplementedError
+
+    def modalities(self) -> List[str]:
+        raise NotImplementedError
+    
+    def output_feature_dim(self) -> int:
+        raise NotImplementedError
+    
+    def set_normalizer(self, normalizer: LinearNormalizer):
+        raise NotImplementedError
